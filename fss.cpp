fbe1ee49729c1a2497cf6a5742c3c01e652f23d2
Aditya-A-garwal <64585012+Aditya-A-garwal@users.noreply.github.com>
2020-05-19 15:05:10 +0530

    adding basic parameter handling

--- fss.cpp
+++ fss.cpp
@@ -70,9 +70,14 @@ int main(int argc, char* argv[]) {
 	cout << "www.dumblebots.com" << endl;					
 	cout << "Begin scanning\n\n" << endl;					
 	
-	cout << argv[1] << endl;					
+	path p;
+	
+	if(argc == 1 || (argv[1])[0] == '-') p = ".";
+	else p = argv[1];
+	
+	cout << p.string() << "\\" << endl;					
 		
-	directory_iterator myDir(argv[1], std::filesystem::directory_options::skip_permission_denied);		    
+	directory_iterator myDir(p, std::filesystem::directory_options::skip_permission_denied);		    
 	printContent(myDir, 0);
 	directory_entry ent;
 
