fcfa20b8d24b8f535fbd3d405b2d4140f26f9ceb
Aditya-A-garwal <64585012+Aditya-A-garwal@users.noreply.github.com>
2020-05-19 17:56:18 +0530

    added program mode for recursively printing directories, leaving out files

--- fss.cpp
+++ fss.cpp
@@ -2,62 +2,110 @@
 #include <iostream>
 #include <filesystem>
 
+#define DEFAULT	0
+#define RECURSE	1	
+#define DIRFILE	2
+
 using namespace std;
 using namespace filesystem;
 
-void prntLatest() {
-
-}
-
-long getSizeOf(directory_iterator iter) {
-	long size = 0;
+long getSizeOf(directory_entry entry) {	
 	
-	directory_entry ent;
+	if(entry.is_directory() == 1) {
+		directory_iterator iter(entry.path());
+				
+		long size = 0;
 
-	while(iter != end(iter)) {
-		ent = *iter;			
-		
-		if(ent.is_directory() == 1) { 
-			size += getSizeOf(directory_iterator(ent.path()));
-		}
-		else {
+		while(iter != end(iter)) {
+			directory_entry ent = *iter;			
+			
 			size += ent.file_size();
-		}		
+			if(ent.is_directory() == 1) size += getSizeOf(ent);
+			
+			iter++;		
+		}
 		
-		iter++;		
+		return size;
 	}
 	
-	return size;
+	else return entry.file_size();	
 	
 }
 
-void printContent(directory_iterator iter, int s) {	
+
+void printContent0(directory_iterator iter, int s) {	
+			
+}
+
+
+void printContent1(directory_iterator iter, int s) {		
+	directory_entry ent;
+	int size = 0;
+	int numFiles = 0;
+
+	while(iter != end(iter)) {
+		ent = *iter;											
+
+	    if(ent.is_directory() == 1) {				
+			cout << getSizeOf(ent);
+			if(s <= 6) { 
+				for(int i = 0; i <= s; i++) cout << "\t";
+			}
+			else {
+				for(int i = 0; i <= 6; i++) cout << "\t";
+				for(int i = 6; i <= s; i++) cout << "..";
+			}		
+			cout << ent.path().stem().string() << "\\\t" << endl;											
+			printContent1(directory_iterator(ent.path()), (s+1));			
+		}		
+		
+		else if(ent.is_directory() != 1) {
+			numFiles++;
+			size += ent.file_size();										
+		}			
+		
+		iter++;			
+	}		
+
+	if(numFiles != 0) {
+		cout << size;
+		if(s <= 6) { 
+			for(int i = 0; i <= s; i++) cout << "\t";
+		}
+		else {
+			for(int i = 0; i <= 6; i++) cout << "\t";
+			for(int i = 6; i <= s; i++) cout << "..";
+		}		
+		cout << "<" << numFiles << " files>\t" << endl;				
+	}
+}
+
+
+void printContent2(directory_iterator iter, int s) {	
 	
 	directory_entry ent;
 
 	while(iter != end(iter)) {
-		ent = *iter;			
-		int size = 0;
+		ent = *iter;						
+		cout << getSizeOf(ent);
 		
 		if(s <= 6) { 
-			for(int i = 0; i <= s; i++) cout << "    ";
+			for(int i = 0; i <= s; i++) cout << "\t";
 		}
 		else {
-			for(int i = 0; i <= 6; i++) cout << "    ";
+			for(int i = 0; i <= 6; i++) cout << "\t";
 			for(int i = 6; i <= s; i++) cout << "..";
 		}
 				
 		cout << ent.path().stem().string();		
 		
 		if(ent.is_directory() != 1) {
-			size = ent.file_size();							
-			cout << ent.path().extension().string() << "\t\t\t" << size << endl;			
+			cout << ent.path().extension().string() << endl;			
 		}
 		
 		else if(ent.is_directory() == 1) {		
-			size = getSizeOf(directory_iterator(ent.path()));
-			cout << "\\\t\t\t" << size << endl;			
-			printContent(directory_iterator(ent.path()), (s+1));			
+			cout << "\\" << endl;			
+			printContent2(directory_iterator(ent.path()), (s+1));			
 		}
 		
 		iter++;		
@@ -78,7 +126,7 @@ int main(int argc, char* argv[]) {
 	cout << p.string() << "\\" << endl;					
 		
 	directory_iterator myDir(p, std::filesystem::directory_options::skip_permission_denied);		    
-	printContent(myDir, 0);
+	printContent1(myDir, 0);
 	directory_entry ent;
 
 }
