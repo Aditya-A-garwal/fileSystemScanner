79632764d7adfe91860a1dc3a40b30b41a63e0db
Aditya-A-garwal <64585012+Aditya-A-garwal@users.noreply.github.com>
2020-05-21 19:04:06 +0530

    Combined printContentA, printContentB to printContent, added number period formatting

--- fss.cpp
+++ fss.cpp
@@ -7,15 +7,29 @@
 using namespace std;
 using namespace filesystem;
 
-void printIndent(int s) {
+string prntNum(unsigned long num) {
+	string toRet = to_string(num);
+	int ctr = 0;
+	
+	for(int i = toRet.length()-1; i >= 0; i--) {
+		if(ctr%3 == 0 && ctr>0) {
+			toRet = toRet.substr(0, (i+1)) + "," + toRet.substr((i+1), toRet.length());
+		}
+		ctr++;		
+	}
+	return toRet;
+}
+
+void printIndent(int s) 
+{
 	for(int i = 0; i <= min(s, 6); i++) cout << "    ";	
 	if(s > 6) {		
 		for(int i = 6; i <= s; i++) cout << "..";
 	}		
 }
 
-long getSizeOf(directory_entry entry) {	
-	
+unsigned long getSizeOf(directory_entry entry) 
+{		
 	if(entry.is_directory() == 1) {
 		directory_iterator iter(entry.path());
 				
@@ -37,93 +51,96 @@ long getSizeOf(directory_entry entry) {
 	
 }
 
-void printContentA(directory_iterator iter, int s, bool recur) {	
-	directory_entry ent;
+void printContent(directory_iterator iter, int s, bool showDir, bool showFile) {
 	unsigned long size = 0;
 	unsigned int numFiles = 0;
+	
+	directory_entry ent;
 
-	while(iter != end(iter)) {
+	while(iter != end(iter)) 
+	{
 		ent = *iter;											
 		
-	    if(ent.is_directory() == 1) {				
-			cout << setfill(' ') << setw(NUMDIGITS) << getSizeOf(ent);
-			printIndent(s);	
-			cout << "<" << ent.path().stem().string() << ">" << endl;		
-			if(recur) printContentA(directory_iterator(ent.path()), (s+1), 1);
+	    if(ent.is_directory() == 1) 
+		{		
+			cout << setfill(' ') << setw(NUMDIGITS) << prntNum(getSizeOf(ent));
+			printIndent(s);
+			cout << "<" << ent.path().stem().string() << ">" << endl;
+			if(showDir) printContent(directory_iterator(ent.path()), (s+1), showDir, showFile);
 		}		
 		
-		else if(ent.is_directory() != 1) {
-			numFiles++;
-			size += ent.file_size();					
-		}
+		else if(ent.is_directory() != 1) 
+		{
+			if(showFile)
+			{
+				cout << setfill(' ') << setw(NUMDIGITS) << prntNum(ent.file_size());
+				printIndent(s);
+				cout << ent.path().stem().string() << ent.path().extension().string() << endl;				
+			}
+			else 
+			{
+				size += ent.file_size();
+				numFiles++;
+			}
+		}		
 		iter++;		
 	}
-	if(numFiles != 0) {
-		cout << setfill(' ') << setw(NUMDIGITS) << size;
+	
+	if(numFiles != 0 && !showFile) {
+		cout << setfill(' ') << setw(NUMDIGITS) << prntNum(size);
 		printIndent(s);
 		cout << "<" << numFiles << " files>\t" << endl;				
 	}	
+	
 }
 
-void printContentB(directory_iterator iter, int s, bool recur) {	
+int main (int argc, char* argv[]) 
+{						
+	bool	showDir = 0;	
+	bool	showFile = 0;			
 	
-	directory_entry ent;
-
-	while(iter != end(iter)) {
-		ent = *iter;	
-		iter++;
-		cout << setfill(' ') << setw(NUMDIGITS) << getSizeOf(ent);			
-				
-		printIndent(s);									
-		
-		if(ent.is_directory() != 1) {		
-			cout << ent.path().stem().string() << ent.path().extension().string() << endl;				
-		}
-		
-		else if(ent.is_directory() == 1) {		
-			cout << "<" << ent.path().stem().string() << ">" << endl;					
-			if(recur) printContentB(directory_iterator(ent.path()), (s+1), 1);			
-		}
-	}			
-}
-
-int main(int argc, char* argv[]) {					
+	path	p = ".";	
 	
-	path p = ".";
-	int mode = 0;		
-	bool isRecur = 0;
-	
-	if(argc >= 2) {
-		for(int i = 1; i < argc; i++) {					
-			if((argv[i])[0] == '-') {				
-				if((argv[i])[1] == 'd') isRecur = 1;
-				else if((argv[i])[1] == 'f') mode = 1;
-				else if((argv[i])[1] == 'h') {
-					cout << "\nfss [-h] [-f] [-d] [path]\n";
-					cout << "fss [-h] [-d] [-f] [path]\n";
-					cout << "fss [-h] [-f] [path] [-d]\n";
-					cout << "fss [-h] [-d] [path] [-f]\n\n";					
-					cout << "-d recursively prints directories\n";
-					cout << "-f expands files\n";
-					cout << "-h for help\n" << endl;
+	if (argc >= 2) 
+	{
+		for(int i = 1; i < argc; i++) 
+		{					
+			if((argv[i])[0] == '-') 
+			{				
+				if((argv[i])[1] == 'd') 
+					showDir = 1;
+				else if((argv[i])[1] == 'f') 
+					showFile = 1;
+				else 
+				{
+					cout << "\nSyntax:\n";
+					cout << "fss [-flag(s)] [path]\n";
+					
+					cout << "\nFlags:\n";					
+					cout << "-d show sub-directories\n";
+					cout << "-f show individual files\n";
+					cout << "-h for help\n";
+					
+					cout << "\nExamples:\n";					
+					cout << "fss (show directories with sizes from current path)\n";
+					cout << "fss -f (show directories and files with sizes from current path)\n";
+					cout << "fss -d -f (show directories, sub-directories and files with sizes from current path)" << endl;					
+					
 					return 0;
 				}
 			}				
-			else {
-				p = argv[i];			
-			}
+			else 
+				p = argv[i];						
 		}			
 	}					
 	
 	cout << "\nFilesystem Scanner v0.9" << endl;					
-	cout << "from dumblebots.com" << endl;							
+	cout << "From dumblebots.com" << endl;								
 	
 	cout << absolute(p).string() << "\\" << endl;					
 		
 	directory_iterator myDir(p);		    
-	
-	if(mode == 0) printContentA(myDir, 0, isRecur);
-	else if(mode == 1) printContentB(myDir, 0, isRecur);
-	directory_entry ent;
+		
+	printContent(myDir, 0, showDir, showFile);
 
 }
